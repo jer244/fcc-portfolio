b3f3bfc0550bbafad5bc108b5c698f1b831c637e
Jeremy <hurleyjer@yahoo.com>
2017-09-11 12:25:29 -0400

    add Random Wiki button

--- client/app/wiki/wiki.component.ts
+++ client/app/wiki/wiki.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Response, Http } from '@angular/http';
+import 'rxjs/Rx';
+import { Observable } from 'rxjs';
+
+import { WikiService } from "app/wiki/wiki.service";
 
 @Component({
   selector: 'fp-wiki',
   templateUrl: './wiki.component.html',
-  styleUrls: ['./wiki.component.scss']
+  styleUrls: ['./wiki.component.scss'],
+  providers: [WikiService]
 })
 export class WikiComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: Http) { }
 
   ngOnInit() {
+  }
+  getRandom(){
+    window.open('https://en.wikipedia.org/wiki/Special:Random', "blank")
   }
 
 }
