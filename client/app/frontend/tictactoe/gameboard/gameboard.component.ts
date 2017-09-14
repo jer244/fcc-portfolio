5d18ba20c080c0de001177b98052b5eda208be4d
Jeremy <hurleyjer@yahoo.com>
2017-09-14 00:25:58 -0400

    working head to head tic tac toe game

--- client/app/frontend/tictactoe/gameboard/gameboard.component.ts
+++ client/app/frontend/tictactoe/gameboard/gameboard.component.ts
@@ -7,14 +7,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameboardComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
+  
   compTurn: boolean = false;
-  cellArray=Array(9).fill(null);
+  player: string = 'X';
+  message: string = '';
+  cellArray = Array(9).fill(null);
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  cellClick(index: number) {
+    if (!this.compTurn && !this.cellArray[index]) {
+      this.cellArray[index] = this.player;
+      if (this.checkWin()) {
+        this.message= this.player + " wins!!";
+        this.resetBoard();
+      } else
+        this.player = this.player == "X" ? "O" : "X";
+    }
+    console.log(this.cellArray);
   }
-  cellClick(id: number){
-    if(!this.compTurn)
-    console.log(id);
+
+  resetBoard() {
+    this.cellArray = Array(9).fill(null);
+    this.player = 'X';
+  }
+
+  checkWin() {
+    const winLines: number[][] = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < 8; i++) {
+      if (this.cellArray[winLines[i][0]] &&
+        (this.cellArray[winLines[i][0]] === this.cellArray[winLines[i][1]]) &&
+        (this.cellArray[winLines[i][1]] === this.cellArray[winLines[i][2]])) {
+        return true;
+      }
+    }
+    return false;
   }
 }
+
