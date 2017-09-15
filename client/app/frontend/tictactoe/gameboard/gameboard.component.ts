fb5704060a5b083d618335b432c3e46c25ba369c
Jeremy <hurleyjer@yahoo.com>
2017-09-15 09:50:29 -0400

    refactor tic tac toe

--- client/app/frontend/tictactoe/gameboard/gameboard.component.ts
+++ client/app/frontend/tictactoe/gameboard/gameboard.component.ts
@@ -1,36 +1,73 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'fp-gameboard',
   templateUrl: './gameboard.component.html',
   styleUrls: ['./gameboard.component.scss']
 })
-export class GameboardComponent implements OnInit {
+export class GameboardComponent {
 
   constructor() {}
-  
+
+  //game state
+  vsComp: boolean = false;  
   compTurn: boolean = false;
+  userSymbol: string = '';
+  compSymbol: string = '';
+  symbols: string[] = ['X', 'O'];
   player: string = 'X';
   message: string = '';
   cellArray = Array(9).fill(null);
 
-  ngOnInit() {}
-
   cellClick(index: number) {
+    console.log('vsComp: ' + this.vsComp + ', compTurn: ' + this.compTurn)
+    //allow move if not computer's turn and cell is empty
     if (!this.compTurn && !this.cellArray[index]) {
       this.cellArray[index] = this.player;
+      //alert and reset if win
       if (this.checkWin()) {
-        this.message= this.player + " wins!!";
+        this.message = this.player + " wins!!";
         this.resetBoard();
-      } else
+        return;
+      } //alert and reset if draw
+      else if (this.checkDraw()) {
+        this.message = "Draw!";
+        this.resetBoard();
+        return;
+      } //switch symbol and make comp move if vs computer
+      else {
         this.player = this.player == "X" ? "O" : "X";
+        if (this.vsComp) {
+          this.compTurn = true;
+          this.compMove();
+        }
+        return;
+      }
     }
     console.log(this.cellArray);
   }
 
-  resetBoard() {
-    this.cellArray = Array(9).fill(null);
-    this.player = 'X';
+  compMove() {
+    if(this.compTurn){
+    for(let i = 0; i < 9; i++) {
+      console.log(this.cellArray[i]);
+      if (!this.cellArray[i]) {
+        this.cellArray[i] = this.player;
+        this.player = this.player == "X" ? "O" : "X";         
+        break;
+      }
+    }
+    if (this.checkWin()) {
+      this.message = this.player + " wins!!";
+      this.resetBoard();
+    }else
+    if (this.checkDraw()) {
+      this.message = "Draw!";
+      this.resetBoard();
+     }
+    this.compTurn = false;
+    return;
+    }
   }
 
   checkWin() {
@@ -42,16 +79,24 @@ export class GameboardComponent implements OnInit {
       [1, 4, 7],
       [2, 5, 8],
       [0, 4, 8],
-      [2, 4, 6]
+      [2, 4, 6],
     ];
-    for (let i = 0; i < 8; i++) {
-      if (this.cellArray[winLines[i][0]] &&
-        (this.cellArray[winLines[i][0]] === this.cellArray[winLines[i][1]]) &&
-        (this.cellArray[winLines[i][1]] === this.cellArray[winLines[i][2]])) {
-        return true;
-      }
-    }
-    return false;
+    return winLines.some((elem, i, array) => { 
+                      return  (this.cellArray[elem[0]] &&
+                              (this.cellArray[elem[0]] === this.cellArray[elem[1]]) &&
+                              (this.cellArray[elem[1]] === this.cellArray[elem[2]]))
+                          })
+  }
+
+  checkDraw() {
+    return this.cellArray.every((elem, index, array)=>{return elem!=null})
+  }
+
+  resetBoard() {
+    this.cellArray.fill(null);
+    this.player = 'X';
+    setTimeout(() => this.message = '', 1000);
+
   }
 }
 
